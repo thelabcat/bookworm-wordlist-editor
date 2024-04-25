2e608fa518db369ac7ce0744bcae01a4346d2bfe
Wilbur Jaywright <zargulthewizard@outlook.com>
2024-04-25 13:09:24 -0400

    Made PyInstaller build Windows Git Bash compatible Changed pyinstaller_build.sh to use curl instead of wget

--- pyinstaller_build.sh
+++ pyinstaller_build.sh
@@ -1,6 +1,5 @@
 echo "Updating the English word frequency list"
-rm large_en.msgpack.gz
-wget https://raw.githubusercontent.com/rspeer/wordfreq/master/wordfreq/data/large_en.msgpack.gz
+curl https://raw.githubusercontent.com/rspeer/wordfreq/master/wordfreq/data/large_en.msgpack.gz -o large_en.msgpack.gz
 echo "Building exe"
 pyinstaller -F --icon=bookworm_wordlist_editor.ico --add-data bookworm_wordlist_editor.png:. --add-data large_en.msgpack.gz:wordfreq/data/ bookworm_wordlist_editor.pyw
 echo "Cleaning up exe build residue"
