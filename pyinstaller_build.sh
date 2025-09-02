ad7c36ff70aa7f2089410ee958781d6be20aece5
Wilbur Jaywright <zargulthewizard@outlook.com>
2025-09-02 16:48:17 -0400

    Added build log saving

--- pyinstaller_build.sh
+++ pyinstaller_build.sh
@@ -1,7 +1,7 @@
 echo "Updating the English word frequency list"
 curl https://raw.githubusercontent.com/rspeer/wordfreq/master/wordfreq/data/large_en.msgpack.gz -o large_en.msgpack.gz
 echo "Building exe"
-pyinstaller -F --icon=bookworm_wordlist_editor.ico --add-data bookworm_wordlist_editor.png:. --add-data large_en.msgpack.gz:wordfreq/data/ bookworm_wordlist_editor.pyw
+pyinstaller -F --icon=bookworm_wordlist_editor.ico --add-data bookworm_wordlist_editor.png:. --add-data large_en.msgpack.gz:wordfreq/data/ bookworm_wordlist_editor.pyw 2>&1 | tee pyinstaller_build_log.txt
 echo "Cleaning up exe build residue"
 rm -rf build
 rm bookworm_wordlist_editor.spec
