f7a79b12a65f02442e9c05d5948c5bc8c669fecb
Wilbur Jaywright <zargulthewizard@outlook.com>
2025-09-03 12:09:35 -0400

    Completed my part of #12
    - A patch for Fedora-specific PyInstaller is in the works
    - App now bundles in a clean virtual environment

--- pyinstaller_build.sh
+++ pyinstaller_build.sh
@@ -1,7 +1,22 @@
+echo "Preparing venv"
+python -m venv .venv
+
+echo "Activating venv"
+source .venv/bin/activate
+
+echo "Installing requirements"
+pip install -r requirements.txt
+pip install -U setuptools pyinstaller
+
 echo "Updating the English word frequency list"
 curl https://raw.githubusercontent.com/rspeer/wordfreq/master/wordfreq/data/large_en.msgpack.gz -o large_en.msgpack.gz
+
 echo "Building exe"
 pyinstaller -F --icon=bookworm_wordlist_editor.ico --add-data bookworm_wordlist_editor.png:. --add-data large_en.msgpack.gz:wordfreq/data/ bookworm_wordlist_editor.pyw 2>&1 | tee pyinstaller_build_log.txt
+
 echo "Cleaning up exe build residue"
 rm -rf build
 rm bookworm_wordlist_editor.spec
+
+echo "Deactivating venv"
+deactivate
