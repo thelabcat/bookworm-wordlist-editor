3c0a34419a7a8024e11f1d93998c79a917e45d33
Wilbur Jaywright <zargulthewizard@outlook.com>
2025-09-11 16:16:17 -0400

    Delete spec file based on output name

--- pyinstaller_build.sh
+++ pyinstaller_build.sh
@@ -43,6 +43,9 @@ else
     source ./.venv/bin/activate
 fi
 
+output_name="bookworm_wordlist_editor-$os_suffix-$(uname -m)"
+echo "Executable output name determined to be $output_name"
+
 echo "Installing requirements"
 pip install -r requirements.txt
 pip install -U setuptools pyinstaller
@@ -51,11 +54,11 @@ echo "Updating the English word frequency list"
 curl https://raw.githubusercontent.com/rspeer/wordfreq/master/wordfreq/data/large_en.msgpack.gz -o large_en.msgpack.gz
 
 echo "Building exe"
-pyinstaller -F --icon=bookworm_wordlist_editor.ico --add-data bookworm_wordlist_editor.png:. --add-data large_en.msgpack.gz:wordfreq/data/ --name "bookworm_wordlist_editor-$os_suffix-$(uname -m)" bookworm_wordlist_editor.pyw 2>&1 | tee pyinstaller_build_log.txt
+pyinstaller -F --icon=bookworm_wordlist_editor.ico --add-data bookworm_wordlist_editor.png:. --add-data large_en.msgpack.gz:wordfreq/data/ --name "$output_name" bookworm_wordlist_editor.pyw 2>&1 | tee pyinstaller_build_log.txt
 
 echo "Cleaning up exe build residue"
 rm -rf build
-rm bookworm_wordlist_editor.spec
+rm "$output_name.spec"
 
 echo "Deactivating venv"
 deactivate
